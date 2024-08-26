3738f6119e7f3554f8f18f8f62fec87287c34cbd
RevathiLagudu <152807558+RevathiLagudu@users.noreply.github.com>
2024-08-26 11:33:49 +0530

    Add files via upload

---
+++ lowerCase/src/main/groovy/com/example/entity/User.groovy
@@ -0,0 +1,33 @@
+package com.example.entity
+
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.serde.annotation.Serdeable
+
+@Introspected
+@Serdeable
+class User {
+    String name
+    String email
+    Long phone
+    String address
+    String password
+
+    User(String name, String password, String address, Long phone, String email) {
+        this.name = name
+        this.password = password
+        this.address = address
+        this.phone = phone
+        this.email = email
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ lowerCase/src/main/groovy/com/example/service/MessageConsumer.groovy
@@ -0,0 +1,18 @@
+package com.example.service
+
+
+import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.configuration.kafka.annotation.Topic
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@KafkaListener(groupId = "example-consumer")
+class MessageConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageConsumer.class)
+
+    @Topic("demo-topic")
+    void receiveMessage(def user ) {
+        LOG.info("Received Message: ${user}")
+    }
+}
